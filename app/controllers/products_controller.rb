56dd6f709ac27c2f1d16c1ec0a31783c17cbe591
kaz4ge <kaz4ge.yamaguchi@gmail.com>
2020-08-16 18:21:19 +0900

    update-controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,20 @@
 class ProductsController < ApplicationController
 
   def index
+    
   end
   
-  def new    
+  def new
+    
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root to: 'users#index'
   resources :products, only: :index
   resources :products, only: :new
+
+  root 'products#index'
+  resources :products, except: :show
 end
